a126283b34e0576b5dbc66483aadc146294b43a6
rawera_daniel <rawera.daniel@gmail.com>
2024-09-12 11:41:29 +0300

    feat: Set up routes and install React Router library

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import GlobalStyles from './styles/GlobalStyle'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import AppLayout from './ui/AppLayout'
 
 export default function App() {
   return (
     <>
       <GlobalStyles />
-      <div>Hellow world</div>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}></Route>
+
+          {/* <Route path="*" element={<PageNotFound />} /> */}
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
